6c513b9277420d4b88c98ba5973ff3671b58721d
Piyush Kadam <kadam.piyush.17it1041@gmail.com>
2021-09-24 15:04:34 +0530

    [Piyush] added ourworlds page

---
+++ src/components/Imagegallery.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class = "image-main">
+        <!-- <img
+          class="gallery-head"
+          src="https://dxaurk9yhilm4.cloudfront.net/images/8812/DJI_0265_RET_2_210405_212842_d169f13de117664acdaa1543841b7c16.jpg"
+        /> -->
+        <div class="image">
+            <img :src="itemlist.image" />
+            <span class = "description"> {{ itemlist.desc1 }}</span>
+            <span class = "description1"> {{ itemlist.desc2 }}</span> 
+        </div>
+    </div>
+
+</template>
+
+<script lang="ts">
+
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+    props: {
+        itemlist : {
+        type : Object
+        },
+    },
+})
+
+export default class Imagegallery extends Vue {
+   
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,56 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+  },
+  {
+    path: '/about-dlc',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/our-worlds',
+    name: 'OurWorlds',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/OurWorlds.vue')
+  },
+  {
+    path: '/experiences',
+    name: 'Experiences',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Experiences.vue')
+  },
+  {
+    path: '/gallery',
+    name: 'Gallery',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Gallery.vue')
+  },
+  {
+    path: '/press',
+    name: 'Press',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Press.vue')
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
